e01b04676a850220439236fd476db2222ecd2f37
13akstjq <13akstjq@naver.com>
2019-07-26 01:49:21 +0900

    develop | preLoading icon and logo

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading, Font, Asset } from "expo";
+import { Ionicons } from "@expo/vector-icons";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  const [loaded, setLoaded] = useState(false);
+
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      await Asset.loadAsync(require("./assets/logo.png"));
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded ? (
+    <Text>Open up App.console.log(); your app!</Text>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
