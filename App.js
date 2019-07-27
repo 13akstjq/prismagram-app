6084a3006f4b0e27e9d3989049bbb723fb8e3498
13akstjq <13akstjq@naver.com>
2019-07-27 13:49:07 +0900

    setup | install import module
    ApolloClient from apllo-boost
    {ApolloProvider} from react-apollo-boost
    AsyncStorage from react-native
    {inMemoryCache} from 'apollo-cache-inmemory
    {persistCache} from 'apollo-cache-persist

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, AsyncStorage } from "react-native";
 import { AppLoading, Font, Asset } from "expo";
 import { Ionicons } from "@expo/vector-icons";
+import { InMemoryCache } from "apollo-cache-inmemory"; //새로운 cache를 만들기 위함.
+import { persistCache } from "apollo-cache-persist"; // 캐시 유지
+import apolloClientOptions from "./apollo";
+import ApolloClient from "apollo-boost"; // ApolloClient를 쉽게 setting하기 위함
+import { ApolloProvider } from "react-apollo-hooks"; //react-apollo에서 react-hooks를 사용하기 위함.
+
 export default function App() {
   const [loaded, setLoaded] = useState(false);
-
+  const [client, setClient] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
         ...Ionicons.font
       });
       await Asset.loadAsync(require("./assets/logo.png"));
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      });
+      setClient(client);
       setLoaded(true);
     } catch (error) {
       console.log(error);
@@ -22,7 +38,9 @@ export default function App() {
   }, []);
 
   return loaded ? (
-    <Text>Open up App.console.log(); your app!</Text>
+    <ApolloProvider client={client}>
+      <Text>Open up App..log(); your app!</Text>
+    </ApolloProvider>
   ) : (
     <AppLoading />
   );
