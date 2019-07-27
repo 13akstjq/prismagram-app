d2796b597e28d64a3acbf22e2fcc1863db027d92
13akstjq <13akstjq@naver.com>
2019-07-27 14:34:19 +0900

    setup | isloggedIn check

--- App.js
+++ App.js
@@ -7,25 +7,45 @@ import { persistCache } from "apollo-cache-persist"; // 캐시 유지
 import apolloClientOptions from "./apollo";
 import ApolloClient from "apollo-boost"; // ApolloClient를 쉽게 setting하기 위함
 import { ApolloProvider } from "react-apollo-hooks"; //react-apollo에서 react-hooks를 사용하기 위함.
-
+import { ThemeProvider } from "styled-components";
+import Theme from "./Styles/Theme";
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null); // true,false를 구분하기 위해 null로 초기화
   const preLoad = async () => {
     try {
+      //load font
       await Font.loadAsync({
         ...Ionicons.font
       });
+
+      //load asset
       await Asset.loadAsync(require("./assets/logo.png"));
+
+      //create cache
       const cache = new InMemoryCache();
+
+      //persist cache
       await persistCache({
         cache,
         storage: AsyncStorage
       });
+
+      //create apolloClient
       const client = new ApolloClient({
         cache,
         ...apolloClientOptions
       });
+
+      //check islogin
+      const isLoggedIn = AsyncStorage.getItem("isLoggedIn");
+      if (isLoggedIn === null || isLoggedIn === false) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
+
       setClient(client);
       setLoaded(true);
     } catch (error) {
@@ -33,14 +53,17 @@ export default function App() {
     }
   };
 
+  //component did mount
   useEffect(() => {
     preLoad();
   }, []);
 
-  return loaded ? (
-    <ApolloProvider client={client}>
-      <Text>Open up App..log(); your app!</Text>
-    </ApolloProvider>
+  return loaded && client && isLoggedIn !== null ? (
+    <ThemeProvider theme={Theme}>
+      <ApolloProvider client={client}>
+        <Text>Open up App..log(); your app!</Text>
+      </ApolloProvider>
+    </ThemeProvider>
   ) : (
     <AppLoading />
   );
